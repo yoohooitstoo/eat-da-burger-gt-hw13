ecbd209a3e6db62e2721750f5eda8a53549b979c
yoohooitstoo <stumanscott@gmail.com>
2020-07-13 17:35:04 -0400

    created all the folders and started building the server and db

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+-- Insert a set of burgers.
+INSERT INTO burgers (burger_name) VALUES ('hamburger.';
+INSERT INTO burgers (burger_name) VALUES ('double cheese burger.');
+INSERT INTO burgers (burger_name) VALUES ('doublebacon chedaburger.');
