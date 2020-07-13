c223b101d91cc7806daf68b6112840fe5641a7f6
yoohooitstoo <stumanscott@gmail.com>
2020-07-13 19:36:16 -0400

    updated the orm.js and the schema

---
+++ config/orm.js
@@ -0,0 +1,15 @@
+// Import MySQL connection.
+var connection = require("../config/connection.js");
+
+
+
+
+
+
+
+
+
+
+
+// Export the orm object for the model (cat.js).
+module.exports = orm;
--- db/schema.sql
+++ db/schema.sql
@@ -5,7 +5,7 @@ CREATE DATABASE burgers_db;
 -- Makes it so all of the following code will affect burgers_db --
 USE burgers_db;
 
--- Creates the table "people" within animals_db --
+-- Creates the table "burgers" within animals_db --
 CREATE TABLE burgers (
   id int NOT NULL AUTO_INCREMENT,
 	burger_name varchar(255) NOT NULL,
