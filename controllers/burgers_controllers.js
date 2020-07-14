7e84262bab372941cacfb3c40cf0552629666370
yoohooitstoo <stumanscott@gmail.com>
2020-07-13 20:43:12 -0400

    updated the orm.js

---
+++ controllers/burgers_controllers.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const router = express.Router();
+
+// Import the model (cat.js) to use its database functions.
+const burger = require("../models/burger");
+
+// Create all our routes and set up logic within those routes where required.
+router.get("/", function(req, res) {
+  cat.all(function(data) {
+    var hbsObject = {
+      burger: data
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+  });
+});
