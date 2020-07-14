e193e001bf89593b97ba19f2b51aad4a4cee5989
yoohooitstoo <stumanscott@gmail.com>
2020-07-14 17:46:33 -0400

    fixed issues with my syling, handlebars, and my jquery file

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,44 +1,57 @@
-// // Make sure we wait to attach our handlers until the DOM is fully loaded.
+
 // $(function() {
 //     $(".change-devoured").on("click", function(event) {
 //       var id = $(this).data("id");
-//       var newSleep = $(this).data("newsleep");
-  
-//       var newSleepState = {
-//         sleepy: newSleep
-//       };
-  
+//       console.log(id);
 //       // Send the PUT request.
-//       $.ajax("/api/cats/" + id, {
+//       $.ajax("/api/burgers/" + id, {
 //         type: "PUT",
 //       }).then(
 //         function() {
-//           console.log("you devoured your burger");
+//           console.log("here is your burger");
 //           // Reload the page to get the updated list
 //           location.reload();
-//         }
-//       );
-//     });
-  
-//     $(".create-form").on("submit", function(event) {
-//       // Make sure to preventDefault on a submit event.
-//       event.preventDefault();
-  
-//       var newCat = {
-//         name: $("#ca").val().trim(),
-//         sleepy: $("[name=sleepy]:checked").val().trim()
-//       };
-  
-//       // Send the POST request.
-//       $.ajax("/api/cats", {
-//         type: "POST",
-//         data: newCat
-//       }).then(
-//         function() {
-//           console.log("created new cat");
-//           // Reload the page to get the updated list
-//           location.reload();
-//         }
-//       );
-//     });
+//     })
 // });
+// });
+$(function() {
+$(".change-devoured").on("click", function(event) {
+    var id = $(this).data("id");
+    var newDevoured = $(this).data("devoured");
+
+    var newDevouredState = {
+      devoured: newDevoured
+    };
+
+    // Send the PUT request.
+    $.ajax("/api/burgers/" + id, {
+      type: "PUT",
+      data: newDevouredState
+    }).then(
+      function() {
+        console.log("changed devoured to", newDevoured);
+        // Reload the page to get the updated list
+        location.reload();
+  });
+})
+
+  
+    $(".create-form").on("submit", function(event) {
+      // Make sure to preventDefault on a submit event.
+      event.preventDefault();
+  
+      var newBurger = {
+        burger_name: $("[name=burger]").val().trim(),
+      };
+      // Send the POST request.
+      $.ajax("/api/burgers", {
+        type: "POST",
+        data: newBurger
+      }).then(
+        function() {
+          console.log("created new burger");
+          // Reload the page to get the updated list
+          location.reload();
+        });
+    });
+});
