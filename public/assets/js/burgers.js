bfea05c6fee830f00c5f024fd298c5dfed6aac71
yoohooitstoo <stumanscott@gmail.com>
2020-07-14 10:02:53 -0400

    started working on the jquery functionality

---
+++ public/assets/js/burgers.js
@@ -0,0 +1,44 @@
+// // Make sure we wait to attach our handlers until the DOM is fully loaded.
+// $(function() {
+//     $(".change-devoured").on("click", function(event) {
+//       var id = $(this).data("id");
+//       var newSleep = $(this).data("newsleep");
+  
+//       var newSleepState = {
+//         sleepy: newSleep
+//       };
+  
+//       // Send the PUT request.
+//       $.ajax("/api/cats/" + id, {
+//         type: "PUT",
+//       }).then(
+//         function() {
+//           console.log("you devoured your burger");
+//           // Reload the page to get the updated list
+//           location.reload();
+//         }
+//       );
+//     });
+  
+//     $(".create-form").on("submit", function(event) {
+//       // Make sure to preventDefault on a submit event.
+//       event.preventDefault();
+  
+//       var newCat = {
+//         name: $("#ca").val().trim(),
+//         sleepy: $("[name=sleepy]:checked").val().trim()
+//       };
+  
+//       // Send the POST request.
+//       $.ajax("/api/cats", {
+//         type: "POST",
+//         data: newCat
+//       }).then(
+//         function() {
+//           console.log("created new cat");
+//           // Reload the page to get the updated list
+//           location.reload();
+//         }
+//       );
+//     });
+// });
